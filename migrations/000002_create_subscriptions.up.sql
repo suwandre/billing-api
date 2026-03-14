c5ae10a266affe79292fa33253b592cadb25e68b
suwandre <suwandresukijat@gmail.com>
2026-03-14 23:05:16 +0100

    fix: syntax for creating tables in migrations

--- migrations/000002_create_subscriptions.up.sql
+++ migrations/000002_create_subscriptions.up.sql
@@ -1,6 +1,6 @@
-CREATE TABLE subscriptions {
+CREATE TABLE subscriptions (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-}
+)
--- migrations/000003_create_subscription_pricings.up.sql
+++ migrations/000003_create_subscription_pricings.up.sql
@@ -1,6 +1,6 @@
-CREATE TABLE subscription_pricings {
+CREATE TABLE subscription_pricings (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   subscription_id UUID NOT NULL REFERENCES subscriptions(id) ON DELETE CASCADE,
   type SMALLINT NOT NULL,
   price DECIMAL(10, 2) NOT NULL
-}
+)
